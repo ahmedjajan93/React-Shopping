35ed6a7b1cdaf329e31da73a99b7df5a2e47edcd
ahmedjajan93 <ahmedjajan93@gmail.com>
2022-01-09 06:37:45 -0800

    starter react project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/header/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      My App
+      <i className="fas fa-shopping-cart    "></i>
+
+    </div>
+  );
+}
+
+export default App;
